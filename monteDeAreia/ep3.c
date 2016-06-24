351b395fa713af0df2faee2285039fa9b56a7941
pedro <pedro.alexandre@usp.br>
2016-06-24 01:27:27 -0300

    criada a funcao de espalhamento

--- monteDeAreia/ep3.c
+++ monteDeAreia/ep3.c
@@ -39,9 +39,50 @@ int main()
   imprima_tabuleiro(tabuleiro,nlin,ncol);
   return 0;
 }
+/*no comeco todos vizinhos estao habilitados
+checa esq, dir, cim, baixo e desabilita caso necessario*/
+void espalhe_celula(int tabuleiro[MAX][MAX], int i, int j, int nlin, int ncol)
+{
+    int esq,dir,cima,baixo;
+    esq = dir = cima = baixo = 1;
+    if(i == 0)
+      cima = 0;
+    if(j == 0)
+      esq = 0;
+    if(i == nlin+1)
+      baixo = 0;
+    if(j == ncol+1)
+      dir = 0;
+    if(dir == 1){
+      tabuleiro[i][j+1]++;
+    }
+    if(esq == 1){
+      tabuleiro[i][j-1]++;
+    }
+    if(cima == 1){
+      tabuleiro[i-1][j]++;
+    }
+    if(baixo == 1){
+      tabuleiro[i+1][j]++;
+    }
+}
 
 int espalhe(int tabuleiro[MAX][MAX], int ativacao[MAX][MAX],
              int nlin, int ncol, int instante, int *novosativados){
+    int i,j;
+    int copia_tabuleiro[MAX][MAX];
+    copie_matriz(tabuleiro,copia_tabuleiro, nlin, ncol);
+
+    for(i = 0; i < nlin; i++)
+    {
+      for(j = 0; j < ncol; j++)
+      {
+          if(copia_tabuleiro[i][j] >= conta_vizinhos(nlin,ncol,i,j))
+          {
+            espalhe_celula(tabuleiro,i,j,nlin,ncol);
+          }
+      }
+    }
     return 1;
 }
 
